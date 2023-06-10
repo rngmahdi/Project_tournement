c301134455db8924ca9a287b2dbc26c92adf4e3d
rngmahdi <122473093+rngmahdi@users.noreply.github.com>
2023-06-10 21:49:34 +0100

    first button

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-from tkinter import *
+from tkinter import * 
+from tkinter import font
+from tkinter.font import Font
+from tkinter import ttk
 import customtkinter
+
+root = Tk()
+root.title("Tournament manegements")
+root.state("zoomed")
+root.geometry("600x700")
+# root.update_idletasks() 
+########################################################################
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+
+buttonFont = Font(family="Tekton Pro",
+                       size=40,
+                       weight="bold",
+                       )
+
+button = customtkinter.CTkButton(root, text="Create new tournament",
+                                 font=("Bahnschrift Light Condensed",40,"bold"))
+button.configure(fg_color="#37CEFE",width=400,height=80)
+button.pack(pady=20,padx=20)
+button.place(x=(width/2)-200 ,y=height/2)
+
+
+root.mainloop()
