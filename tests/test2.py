7938cbd2907a9d629be95dab6b9a10839874d053
rngmahdi <122473093+rngmahdi@users.noreply.github.com>
2023-06-11 14:38:00 +0100

    some tests

---
+++ tests/test2.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+
+
+class Window():
+   def __init__(self,root) -> None:
+      win = Toplevel(root)
+      win.geometry("500x400")
+      win.title("Second Page")
+
+
