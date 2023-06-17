9d1f5f4452c36098bc6145aba6cd4e96351c18ce
HICHAM IAMIRI <89263522+HichamDe@users.noreply.github.com>
2023-06-17 13:26:33 +0100

    tournament creation is ready to use

--- controller/touranement_creation.py
+++ controller/touranement_creation.py
@@ -1,7 +1,5 @@
 import customtkinter 
 import sqlite3
-import tkinter as tk
-from player import Player
 
 
 class TouranementCreationPage(customtkinter.CTk):
@@ -11,17 +9,34 @@ class TouranementCreationPage(customtkinter.CTk):
         self.columnconfigure(0,weight=1)
         self.rowconfigure(0,weight=1)
         self.rowconfigure(1,weight=13)
-            
-        self.title("Create a new Tourenement")
         self.geometry("600x500")
+        self.title("Tournament Manager")
         
+        # Structure
         self.nav = customtkinter.CTkFrame(self,height=40)
         self.main = customtkinter.CTkFrame(self)
         
-        #* Routes
-        self.route_tournament_creation = customtkinter.CTkButton(self.nav,text="+ Tournament")
-        self.route_tournaments_list = customtkinter.CTkButton(self.nav,text="List Tournament")
-        self.route_player = customtkinter.CTkButton(self.nav,text="Players")
+        # Menu
+        self.route_tournament_creation = customtkinter.CTkButton(self.nav,text="+ Tournament",command=self.tournamentPage)
+        self.route_tournaments_list = customtkinter.CTkButton(self.nav,text="List Tournament",command=self.listTournament)
+        self.route_player = customtkinter.CTkButton(self.nav,text="Players",command=self.playersPage)
+        
+        # Current Active Page
+        # self.tournamentPage()
+        
+        self.nav.grid(row=0,column=0,sticky="new")
+        self.main.grid(row=1,column=0,sticky="news")
+        
+        self.route_tournament_creation.grid(row=0,column=0,padx=5)
+        self.route_tournaments_list.grid(row=0,column=1,padx=5)
+        self.route_player.grid(row=0,column=2,padx=5)
+        
+        self.mainloop()
+        
+    # Methods
+    def tournamentPage(self):
+        
+        self.cleanContainer(self.main)
         
         self.fullName = customtkinter.CTkEntry(self.main,placeholder_text="fullName")
         self.title = customtkinter.CTkEntry(self.main,placeholder_text="title")
@@ -37,36 +52,36 @@ class TouranementCreationPage(customtkinter.CTk):
         self.date.grid()
         self.btn1.grid()
         
-        self.route_tournament_creation.grid(row=0,column=0)
-        self.route_tournaments_list.grid(row=0,column=1)
-        self.route_player.grid(row=0,column=2)
         
-        self.nav.grid(row=0,column=0,sticky="new")
-        self.main.grid(row=1,column=0,sticky="news")
-        
-        self.mainloop()
-        
-    # Methods
+    def listTournament(self):
+        self.cleanContainer(self.main)
+    
+    def playersPage(self):
+        self.cleanContainer(self.main)
+    
     def createNewTournament(self):
-        
-        fullName = self.fullName.get()
-        title = self.title.get()
-        type = self.type.get()
-        place = self.place.get()
-        date = self.date.get()
-        
+        try:
+            fullName = self.fullName.get()
+            title = self.title.get()
+            type = self.type.get()
+            place = self.place.get()
+            date = self.date.get()
+            
 
-        connect = sqlite3.connect("./database/database.db")
-        cursor = connect.cursor()
+            connect = sqlite3.connect("./database/database.db")
+            cursor = connect.cursor()
 
-        cursor.execute("INSERT INTO tournament (title,place,date,name_of_creator,type) VALUES (?,?,?,?,?)",(title,place,date,fullName,type))
-        connect.commit()
-        connect.close()
-        
-    def player(self):
-        Player(self)
+            # cursor.execute("INSERT INTO tournament (title,place,date,name_of_creator,type) VALUES (?,?,?,?,?)",(title,place,date,fullName,type))
+            connect.commit()
+            connect.close()
+            print("Created")
+        except:
+            print("Failed")
     
     def destroy(self):
         return self.quit()
-    
+
+    def cleanContainer(self,container):
+        for widget in container.winfo_children(): 
+            widget.destroy()
 TouranementCreationPage()
